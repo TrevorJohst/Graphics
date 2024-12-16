97435a809dc3daf0efea125d9669ffcb46b62de2
Trevor Johst <trejohst@mit.edu>
2024-12-16 00:52:28 -0500

    Change all files to use tabs

--- Graphics/Display.cpp
+++ Graphics/Display.cpp
@@ -3,22 +3,22 @@
 //////////////////////////////////////////////////////////////////
 // Creates a display object storing a graphics reference
 Display::Display(Graphics gfx)
-	:
-	graphics(gfx)
+    :
+    graphics(gfx)
 {}
 
 //////////////////////////////////////////////////////////////////
 // Public function that completes all frame logic
 void Display::DisplayFrame()
 {
-	ProcessFrame();
-	graphics.Update();
+    ProcessFrame();
+    graphics.Update();
 }
 
 //////////////////////////////////////////////////////////////////
 // Any changes to the frame happen within here
 void Display::ProcessFrame()
 {
-	//graphics.DrawTriangle({ 200, 200 }, { 300, 400 }, { 350, 150 }, { 0xffffff });
-	graphics.DrawRectangle({ 200, 200 }, { 100, 100 }, { 0xffffff });
+    //graphics.DrawTriangle({ 200, 200 }, { 300, 400 }, { 350, 150 }, { 0xffffff });
+    graphics.DrawRectangle({ 200, 200 }, { 100, 100 }, { 0xffffff });
 }
